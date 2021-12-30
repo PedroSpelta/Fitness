b25df89f9820e7c15ea2ec35a8eb7fa9fd476a6e
Pedro Spelta <p.h.s.8989@hotmail.com>
2021-12-30 02:52:13 -0300

    mudando o grafico de macro para pie

--- components/Daily.tsx
+++ components/Daily.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useFoodContext } from "../context/foodContext";
 import { usePersonalDataContext } from "../context/personalData";
-import { defaultTodayMeals } from "../libs/constants";
 import MacroCircle from "./MacroCircle";
 
 function Daily() {
   const { calories, macrosPerDay } = usePersonalDataContext();
   const { todayMeals } = useFoodContext();
-  
+
   const [consumedMacros, setConsumedMacros] = useState({
     prot: 0,
     carb: 0,
@@ -27,7 +26,6 @@ function Daily() {
     let fat = 0;
     for (let m = 0; m < todayMeals.length; m += 1) {
       const { ingredients } = todayMeals[m];
-
       for (let i = 0; i < ingredients.length; i += 1) {
         carb += ingredients[i].carb;
         prot += ingredients[i].prot;
@@ -42,35 +40,33 @@ function Daily() {
   )}%`;
 
   return (
-    <div className="border border-gray-400 max-w-3xl w-full p-5 mt-8">
-      <div className="flex justify-between w-full">
-        <div>
-          <p className="text-sm text-gray-400">Restam:</p>
-          <span className="text-4xl font-semibold">
-            {personalData.calories - calories}
-          </span>
-          <span className="text-xs text-gray-500">kcal</span>
+    <div className="border border-gray-400 max-w-3xl w-full p-5 mt-8 grid grid-cols-4">
+      <div className="w-full col-span-3">
+        <div className="flex justify-around w-full">
+          <div>
+            <p className="text-sm text-gray-400">Restam:</p>
+            <span className="text-4xl font-semibold">
+              {personalData.calories - calories}
+            </span>
+            <span className="text-xs text-gray-500">kcal</span>
+          </div>
+          <div>
+            <p className="text-sm text-gray-400">Meta:</p>
+            <span className="text-4xl font-semibold">
+              {personalData.calories}
+            </span>
+            <span className="text-xs text-gray-500">kcal</span>
+          </div>
         </div>
-        <div className=" flex justify-end">
-          <MacroCircle
-            completed={consumedMacros.prot}
-            missing={personalData.protein - consumedMacros.prot}
-          />
-          <MacroCircle
-            completed={consumedMacros.carb}
-            missing={personalData.carbo - consumedMacros.carb}
-          />
-          <MacroCircle
-            completed={consumedMacros.fat}
-            missing={personalData.fat - consumedMacros.fat}
-          />
+        <div className="w-full border-gray-900 bg-gray-300 rounded-md">
+          <div
+            className={`mt-3 h-6 bg-gray-900 rounded-md`}
+            style={{ width: dailyCalWidth }}
+          ></div>
         </div>
       </div>
-      <div className="w-full border-gray-900 bg-gray-300 rounded-md">
-        <div
-          className={`mt-3 h-6 bg-gray-900 rounded-md`}
-          style={{ width: dailyCalWidth }}
-        ></div>
+      <div className="flex justify-center items-center">
+        <MacroCircle macros={consumedMacros} />
       </div>
     </div>
   );
--- components/MacroCircle.tsx
+++ components/MacroCircle.tsx
@@ -1,32 +1,40 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-function MacroCircle({
-  completed,
-  missing,
-}: {
-  completed: number;
-  missing: number;
-}) {
+interface IMacrosObject {
+  carb: number;
+  prot: number;
+  fat: number;
+}
+
+function MacroCircle({ macros }: { macros: IMacrosObject }) {
   const dougData = {
-    labels: [],
+    labels: ["Proteína", "Carboidrato", "Gordura"],
     datasets: [
       {
         label: "# of Votes",
-        data: [completed, missing],
-        backgroundColor: ["rgba(54, 162, 235, 0.8)", "rgba(255, 99, 132, 0.8)"],
-        borderColor: ["rgba(0,0,0,0.1)"],
-        borderWidth: 0.5
+        data: [macros.prot, macros.carb, macros.fat],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.8)",
+          "rgba(54, 162, 235, 0.8)",
+          "rgba(255, 206, 86, 0.8)",
+        ],
+        borderColor: [
+          "rgba(0, 0, 0, 0.3)",
+          "rgba(0, 0, 0, 0.3)",
+          "rgba(0, 0, 0, 0.3)",
+        ],
+        borderWidth: 0.5,
       },
     ],
   };
 
   const dougOptions = {
     // for a semi circle
-    rotation: -90,
-    circumference: 180,
+    // rotation: -90,
+    // circumference: 180,
     responsive: true,
     plugins: {
       legend: {
@@ -39,12 +47,12 @@ function MacroCircle({
   };
 
   return (
-    <div className="w-20 h-20 relative">
-      <Doughnut data={dougData} options={dougOptions} />
-      <p className="absolute bottom-3.5 left-1/2 -translate-x-1/2 text-sm text-gray-600 -z-10">
+    <div className="w-32 h-32">
+      <Pie data={dougData} options={dougOptions} />
+      {/* <p className="absolute bottom-3.5 left-1/2 -translate-x-1/2 text-sm text-gray-600 -z-10">
         {" "}
         {missing + completed}
-      </p>
+      </p> */}
     </div>
   );
 }
