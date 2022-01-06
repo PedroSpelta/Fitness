10e5460c44b506b00cd8ccefd95b43d7868c8911
PedroSpelta <p.h.s.8989@hotmail.com>
2022-01-06 06:53:57 -0300

    adicionado o next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { DataWrapper } from "../context/personalData";
 import { FoodContextWrapper } from "../context/foodContext";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <FoodContextWrapper>
-      <DataWrapper>
-        <Component {...pageProps} />
-      </DataWrapper>
-    </FoodContextWrapper>
+    <SessionProvider session={pageProps.session}>
+      <FoodContextWrapper>
+        <DataWrapper>
+          <Component {...pageProps} />
+        </DataWrapper>
+      </FoodContextWrapper>
+    </SessionProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
+import { signIn } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    signIn();
+  }, [])
   return (
     <div className='text-green-500 text-8xl'>hello world</div>
   )
