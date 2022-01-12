56b1e9a5a97b7ac6850620ad6765ac4d52f5050c
PedroSpelta <p.h.s.8989@hotmail.com>
2022-01-12 04:30:37 -0300

    adding cookies

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,18 +1,24 @@
 import { getToken } from "next-auth/jwt";
-import { NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({req, secret: process.env.SECRET, encryption: true})
-  const { pathname} = req.nextUrl;
-  
+  const token = await getToken({
+    req,
+    secret: process.env.SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith("https://") ??
+      !!process.env.VERCEL_URL,
+  });
+  const { pathname } = req.nextUrl;
+
   // permite req se é for uma req para o next-auth ou o token for existente
-  
+
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
-  
-  if (!token && pathname !=="/login") {
-    console.log('not logged');
-    return NextResponse.redirect("/login")
+
+  if (!token && pathname !== "/login") {
+    console.log("not logged");
+    return NextResponse.redirect("/login");
   }
 }
