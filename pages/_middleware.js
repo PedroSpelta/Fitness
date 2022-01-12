fecf4aae62e279db04f474582012fa8de11610ca
PedroSpelta <p.h.s.8989@hotmail.com>
2022-01-12 03:04:30 -0300

    teste encryption

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse} from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({req, secret: process.env.SECRET})
+  const token = await getToken({req, secret: process.env.SECRET, encryption: true})
   const { pathname} = req.nextUrl;
   
   // permite req se é for uma req para o next-auth ou o token for existente
